9f2f44c7e49fd350d91feb1f2c7f70ef4b363d66
biless <biless@qq.com>
2016-12-20 23:31:49 +0800

    初始化项目

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/tidwall/gjson"
 	"fmt"
+	"github.com/tidwall/gjson"
 	_ "httprouter"
+	"net/http"
 )
 
 const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
 
 func main() {
-	value := gjson.Parse(json).Get("name.last")
-	fmt.Println(value)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, gjson.Parse(json).String())
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
